a425fe8d199cb2ad865d0e1a361b0a9c22e9c481
Charlie Lancaster <charlie.lancaster@yousoft.com>
2020-02-15 21:26:05 +0000

    added player and platform collider. commented more code

--- phaser/scripts/create.js
+++ phaser/scripts/create.js
@@ -13,27 +13,32 @@ module.exports = () => {
 		platforms.create(50, 250, 'ground');
 		platforms.create(750, 220, 'ground');
 
-		//Create player and add physics
+		//Create player(sprite) and add physics
 		player = this.physics.add.sprite(100, 450, 'dude');
 
 		//Set bounce after landing
 		player.setBounce(0.2);
 		//Player can't go beyond world bounds
 		player.setCollideWorldBounds(true);
+		//Create a collider between the player and the platforms to allow standing on them
+		this.physics.add.collider(player, platforms);
 
+		//Set left moving animation
 		this.anims.create({
 		    key: 'left',
 		    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
 		    frameRate: 10,
 		    repeat: -1
 		});
-
+		
+		//Set turning animation
 		this.anims.create({
 		    key: 'turn',
 		    frames: [ { key: 'dude', frame: 4 } ],
 		    frameRate: 20
 		});
 
+		//Set right moving animation
 		this.anims.create({
 		    key: 'right',
 		    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
